24aaf9c0c2b0a71d9134919ed3957f9536e8b538
Bilal Khalid <bkhalid0311@gmail.com>
2021-06-02 18:03:08 +0500

    heroku commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
+   :user_name            => "bkhalid0311@gmail.com",
    :password             => "bilalninja456",
    :authentication       => "plain",
   :enable_starttls_auto => true
